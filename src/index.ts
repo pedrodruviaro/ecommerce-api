fbe01f1faff4434fc500a70d981cbf03f58682ef
Pedro Henrique <82953655+pedrodruviaro@users.noreply.github.com>
2025-02-18 14:59:45 -0300

    feat: add firebase web package

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import express from "express"
-import * as admin from "firebase-admin"
+import * as firebaseAdmin from "firebase-admin"
+import * as firebaseApp from "firebase/app"
 import { routes } from "./routes"
 import { errorHandler } from "./middlewares/error-handler.middleware"
 import { pageNotFoundHandler } from "./middlewares/page-not-found.middleware"
 
 const PORT = process.env.PORT || 8000
 
-admin.initializeApp()
+firebaseAdmin.initializeApp()
+firebaseApp.initializeApp({
+  apiKey: process.env.FIREBASE_API_KEY,
+})
 
 const app = express()
 
